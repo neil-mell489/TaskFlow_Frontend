adaa8db8a6a817aba08403903d8bb5199d72bbee
czdigitaldesigner@gmail.com <czdigitaldesigner@gmail.com>
2024-04-13 06:40:49 -0700

    Login is styled and signup styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,28 +18,31 @@ const Login = (props) => {
     };
 
     return (
-        <div className="form-container">
-            <h1>Welcome Back! (to TaskFlow)</h1>
+        <div className="form-container flex flex-col m-10 items-center">
+            <h1 className="pb-10 text-2xl">Welcome Back! (to TaskFlow)</h1>
             <form onSubmit={handleSubmit}>
-                <div>
+                <div className="pb-2">
                     <span>
                         <label htmlFor="username">Username: </label>
-                        <input type="text" name="username" onChange={handleChange}/>
+                        <input type="text" name="username" onChange={handleChange} placeholder="Enter Username"/>
                     </span>
                 </div>
-                <div>
+                <div className="pb-2">
                     <span>                
                         <label htmlFor="email">Email: </label>
-                        <input type="email" name="email" onChange={handleChange}/>
+                        <input type="email" name="email" onChange={handleChange} placeholder="Enter Email"/>
                     </span>
                 </div>
-                <div>
+                <div className="pb-2">
                     <span>                
                         <label htmlFor="password">Password: </label>
-                        <input type="password" name="password" onChange={handleChange}/>
+                        <input type="password" name="password" onChange={handleChange} placeholder="Enter Password"/>
                     </span>
                 </div>
-                <button type="submit">Login</button>
+                <div className="flex justify-center">
+                <button type="submit" className="bg-violet-500 hover:bg-violet-600 text-white p-2 rounded-md m-3">Login</button>                    
+                </div>
+
             </form>
             {/* inside the form div display the error message in red */}
             {errorMsg ? <h4 style={{color: "red"}}>{errorMsg}</h4> : ""}
